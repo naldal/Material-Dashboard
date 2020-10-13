736feb4916dc5ed90857098f0a985d9bbd9d28ea
naldal <cec7378@gmail.com>
2020-10-14 08:26:25 +0900

    destructure object on drawer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import { makeStyles } from '@material-ui/core';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Default from './Default';
 import Drawer from './Drawer.jsx';
 
+const useStyle = makeStyles({
+  container: {
+    display: "flex"
+  },
+})
+
 function App() {
+  const classes = useStyle();
+
   return (
-    <>  
-      <Drawer />
+    <div className={classes.container}>  
+      <Drawer className={classes.drawer} />
       <Switch>
         <Route exact path="/" render={(props) => <Default {...props}/>}></Route>
         <Route></Route>
       </Switch>
-    </>
+    </div>
   );
 }
 
--- src/Drawer.jsx
+++ src/Drawer.jsx
@@ -3,22 +3,35 @@ import {Drawer as MUIDrawer , List,ListItem,ListItemIcon,ListItemText } from '@m
 import { makeStyles } from '@material-ui/core/styles' 
 
 const useStyles = makeStyles({
-
+  drawer: {
+    width: '260px'
+  }
 })
 
 const Drawer = () => {
 
     const classes = useStyles();
+    const itemList = [
+      {text:'aaa'},
+      {text:'aaa'},
+      {text:'aaa'},
+      {text:'aaa'},
+      {text:'aaa'},
+      {text:'aaa'},
+      {text:'aaa'}
+    ];
 
     return (
-        <MUIDrawer variant="permanent">
+        <MUIDrawer variant="permanent" className={classes.drawer}>
           <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            {itemList.map((item, index) => {
+              const { text } = item;
+              return (
               <ListItem button key={text}>
                 <ListItemIcon></ListItemIcon>
                 <ListItemText primary={text} />
               </ListItem>
-            ))}
+            )})}
           </List>
         </MUIDrawer>
     );
