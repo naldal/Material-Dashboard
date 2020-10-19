04205e9cb77101709401ad496ee7a536b5209388
naldal <cec7378@gmail.com>
2020-10-19 23:48:09 +0900

    Updates

---
+++ precache-manifest.187dd317ddafb16d10800668a657d613.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02badf9d50c66275792defad0f1e4ac",
+    "url": "/Material-Dashboard/index.html"
+  },
+  {
+    "revision": "943b76612fe006275213",
+    "url": "/Material-Dashboard/static/js/2.4caff4c8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Material-Dashboard/static/js/2.4caff4c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0fbe367e0d1724ef707",
+    "url": "/Material-Dashboard/static/js/main.cc1919c3.chunk.js"
+  },
+  {
+    "revision": "e25c4d00b27bc6d9c496",
+    "url": "/Material-Dashboard/static/js/runtime-main.02c6ba8a.js"
+  }
+]);
